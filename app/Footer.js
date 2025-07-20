22c39231ff85b0c878e456d533f7b1073588563b
codelife123 <nodedevloper@gmail.com>
2025-07-20 20:50:33 +0530

    Update copyright year in Footer component from 2024 to 2025 for accuracy.

--- app/Footer.js
+++ app/Footer.js
@@ -64,7 +64,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-300">
-          <p>&copy; 2024 Ranga ArcTech. All rights reserved.</p>
+          <p>&copy; 2025 Ranga ArcTech. All rights reserved.</p>
         </div>
       </div>
     </footer>
